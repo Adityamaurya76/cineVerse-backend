d89827b00ad410011fd3395c83e272d89afd052a
Aditya Maurya <aditya@hipster-dev.com>
2025-11-28 15:17:21 +0800

    Fix Render deployment configuration

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config({
 });
 
 import cloudinary from './config/cloudinary.js';
-const port = process.env.PORT;
+const port = Number(process.env.PORT) || 3000;
 
 connectDB().then(() => {
    app.listen(port, () => {
